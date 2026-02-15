1373e997207046fddf4d54350af77ed1fd97280c
Vijay Kammar <vijaykammar10@gmail.com>
2026-02-15 21:15:47 +0530

    added todo app

---
+++ app.js
@@ -0,0 +1,34 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul=document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+    let li=document.createElement("li");
+    li.innerText=inp.value;
+    let delBtn=document.createElement("button");
+    delBtn.classList.add("delete");
+    delBtn.innerText="delete";
+    
+
+    li.appendChild(delBtn);
+    let div=document.createElement("div");
+    div.style.backgroundColor="rgb(50, 50, 105)";
+    div.style.margin="10px";
+    div.classList.add("car");
+
+    div.appendChild(li);
+    ul.appendChild(div);
+    inp.value='';
+
+
+
+});
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let del=event.target.parentElement;
+        let par=del.parentElement;
+        par.remove();
+    }
+    
+})
